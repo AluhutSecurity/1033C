c6966538877bcc309874c20a564ba70786dfe56c
Snaep <ksnaep@web.de>
2015-11-18 13:42:45 +0100

    added

---
+++ 1033C/Utils/MyServerParameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1033C.Utils {
+    public class MyServerParameter {
+        public int Port { get; set; }
+        public System.Net.IPAddress LocalIP { get; set; }
+    }
+}
