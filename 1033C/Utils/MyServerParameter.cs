88e011dec72eac6ad0215649faaf1e997f6c5723
Snaep <ksnaep@web.de>
2015-11-18 13:58:13 +0100

    added constructor for 2 arguments

--- 1033C/Utils/MyServerParameter.cs
+++ 1033C/Utils/MyServerParameter.cs
@@ -8,5 +8,14 @@ namespace _1033C.Utils {
     public class MyServerParameter {
         public int Port { get; set; }
         public System.Net.IPAddress LocalIP { get; set; }
+
+        public MyServerParameter() {
+            this.LocalIP = System.Net.IPAddress.Any;
+        }
+
+        public MyServerParameter( System.Net.IPAddress addr, int port ) {
+            this.LocalIP = addr;
+            this.Port = port;
+        }
     }
 }
